a6f1d28752d703bb51cdb26a30c3aea94e44912d
Satyam Gupta <satyam1720jp@gmail.com>
2023-07-23 11:25:38 +0530

    #

--- Models/ViewModel.cs
+++ Models/ViewModel.cs
@@ -7,14 +7,22 @@ namespace MachineTest.Models
     {
         [Key]
         public int productid { get; set; }
-        [Required]
-        public string productname { get; set; } = default!;
+        [Required(ErrorMessage ="Please Enter Product Name")]
+        [Display(Name = "Product Name :")]
+        public string productname { get; set; }
 
+        [Display(Name = "Category ID :")]
+        [Required(ErrorMessage = "Please Select Category")]
         public int categoryid { get; set; }
         [ForeignKey("categoryid")]
+       
+
         public category category { get; set; } = default!;
 
         [Required]
+        [Display(Name = "Category Name :")]
         public string categoryname { get; set; } = default!;
+       
     }
 }
+
